a3595a2e0ab5b4685cb2828661b61baeacfc4476
Ad De <ekhtrash@gmail.com>
2017-01-30 16:11:49 +0100

    car on moving ground

---
+++ vertical/car.go
@@ -0,0 +1,35 @@
+components {
+  id: "script"
+  component: "/vertical/car.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/vertical/gfx/car.atlas\"\n"
+  "default_animation: \"mustang_C\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
