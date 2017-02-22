426e24b9daee8dbb8317d52de7646d7027cf4cf9
Ad De <ekhtrash@gmail.com>
2017-02-22 13:07:38 +0100

    improved enemy speed

--- vertical/game_objects/enemy/enemy.go
+++ vertical/game_objects/enemy/enemy.go
@@ -12,11 +12,52 @@ components {
     z: 0.0
     w: 1.0
   }
+  properties {
+    id: "speed"
+    value: "40.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "speed_slow"
+    value: "18.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
 }
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_KINEMATIC\nmass: 0.0\nfriction: 0.1\nrestitution: 0.5\ngroup: \"enemy\"\nmask: \"hero\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_BOX\n    position {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 3\n  }\n  data: 150.18515\n  data: 364.1992\n  data: 10.0\n}\nlinear_damping: 0.0\nangular_damping: 0.0\nlocked_rotation: false\n"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"enemy\"\n"
+  "mask: \"hero\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 150.18515\n"
+  "  data: 364.1992\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
   position {
     x: 0.0
     y: 0.0
@@ -32,7 +73,11 @@ embedded_components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/vertical/assets/gfx/enemies/enemies.atlas\"\ndefault_animation: \"mustang_C\"\n"
+  data: "tile_set: \"/vertical/assets/gfx/enemies/enemies.atlas\"\n"
+  "default_animation: \"mustang_C\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 0.0
     y: 0.0
