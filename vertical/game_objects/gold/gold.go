0219ed9499ddb4145dc3119df10f3b9c2e69ce24
Ad De <ekhtrash@gmail.com>
2017-03-06 16:42:35 +0100

    spawn gold

---
+++ vertical/game_objects/gold/gold.go
@@ -0,0 +1,35 @@
+components {
+  id: "script"
+  component: "/vertical/game_objects/gold/gold.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/vertical/assets/gfx/gold/gold.atlas\"\n"
+  "default_animation: \"PNGPIX-COM-Money-Bag-PNG-Transparent-Image-500x575\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
