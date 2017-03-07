5e00def8dc6a30011686626fadb28c5c60ce93c2
Ad De <ekhtrash@gmail.com>
2017-03-07 03:10:05 +0100

    play start engine

--- vertical/game_objects/player/player.go
+++ vertical/game_objects/player/player.go
@@ -101,3 +101,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "start_engine"
+  type: "sound"
+  data: "sound: \"/vertical/assets/sfx/start game2_windows440.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
