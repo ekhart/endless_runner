38756557afda9ad9c644771bfb80b4ddc4556fd3
Ad De <ekhtrash@gmail.com>
2017-03-07 03:41:53 +0100

    credits buttons click sfx

--- vertical/assets/utils.lua
+++ vertical/assets/utils.lua
@@ -4,4 +4,19 @@ function M.play_sound(id)
 	msg.post("#" .. id, "play_sound")
 end
 
+function M.play_sprite_animation(animation)
+  msg.post("#sprite", "play_animation", { id = hash(animation) })
+end
+
+function M.is_id(id, to_hash)
+  return id == hash(to_hash)
+end
+
+function M.click_button(domain, node, action, after)
+	if gui.pick_node(gui.get_node(node), action.x, action.y) then
+		msg.post(domain .. ":/hud#button", "play_sound")
+		after()
+	end
+end
+
 return M
